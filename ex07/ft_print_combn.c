38fef13fd8ea0d9ade72320d3304d5ce9ec86f33
NAKEDI ALPHABERT MALOPE <nmalope@c4r15s1.wethinkcode.co.za>
2020-06-25 11:37:26 +0200

    ex07

---
+++ ex07/ft_print_combn.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putchar(char c)
+{
+	write(1 , &c, 1);
+}
+int ft_print_combn(int n)
+{
+	write(1 , &n, 1);
+}
+
+int ft_print_combn(void)
+{
+
+	int i;
+	int 10
+
+	return(0);
+
+}
+
+int main(void)
+
+{
+	return(0);
+}
+
